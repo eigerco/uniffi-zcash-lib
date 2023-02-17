55ac27b3bda19ef9d06d567d8f9b6ca25920d470
Łukasz Niemier <lukasz@eiger.co>
2023-02-17 12:48:39 +0100

    ft(elixir): implement example functions for BEAM interface

--- lib/uniffi_backend_keys/src/beam.rs
+++ lib/uniffi_backend_keys/src/beam.rs
@@ -1,24 +1,49 @@
-use rustler::{Binary, ResourceArc, NifResult, Error};
+use rustler::{Binary, Error, NifRecord, NifResult, ResourceArc};
 
+#[derive(NifRecord)]
+#[tag = "zcash_priv_key"]
+struct PrivKey(ResourceArc<crate::ZcashAccountPrivKey>);
 
+#[derive(NifRecord)]
+#[tag = "zcash_pub_key"]
+struct PubKey(ResourceArc<crate::ZcashAccountPubKey>);
 
 #[rustler::nif]
-fn from_bytes(data: Binary) -> NifResult<Option<ResourceArc<crate::ZcashAccountPrivKey>>> {
-    if data.len() < 32 { return Err(Error::BadArg) }
+fn from_bytes(data: Binary) -> NifResult<Option<PrivKey>> {
+    if data.len() < 32 {
+        return Err(Error::BadArg);
+    }
 
-    Ok(crate::from_bytes(data.to_vec()).map(ResourceArc::new))
+    Ok(crate::from_bytes(data.to_vec())
+        .map(ResourceArc::new)
+        .map(PrivKey))
 }
 
 #[rustler::nif]
-fn from_seed(seed: Binary, account_id: u32) -> NifResult<Option<ResourceArc<crate::ZcashAccountPrivKey>>> {
-    if seed.len() < 32 { return Err(Error::BadArg) }
+fn from_seed(seed: Binary, account_id: u32) -> NifResult<Option<PrivKey>> {
+    if seed.len() < 32 {
+        return Err(Error::BadArg);
+    }
 
-    Ok(crate::from_seed(seed.to_vec(), account_id).map(ResourceArc::new))
+    Ok(crate::from_seed(seed.to_vec(), account_id)
+        .map(ResourceArc::new)
+        .map(PrivKey))
 }
 
 #[rustler::nif]
-fn to_account_pubkey(priv_key: ResourceArc<crate::ZcashAccountPrivKey>) -> ResourceArc<crate::ZcashAccountPubKey> {
-    ResourceArc::new(crate::to_account_pubkey(&priv_key))
+fn test_from_seed(seed: Binary, account_id: u32) -> NifResult<Option<PrivKey>> {
+    if seed.len() < 32 {
+        return Err(Error::BadArg);
+    }
+
+    Ok(crate::test_from_seed(seed.to_vec(), account_id)
+        .map(ResourceArc::new)
+        .map(PrivKey))
+}
+
+#[rustler::nif]
+fn to_account_pubkey(priv_key: PrivKey) -> PubKey {
+    PubKey(ResourceArc::new(crate::to_account_pubkey(&priv_key.0)))
 }
 
 fn load(env: rustler::Env, _: rustler::Term) -> bool {
@@ -28,8 +53,8 @@ fn load(env: rustler::Env, _: rustler::Term) -> bool {
     true
 }
 
-rustler::init!("Elixir.ZcashBackendKeys", [
-    from_bytes,
-    from_seed,
-    to_account_pubkey
-], load = load);
+rustler::init!(
+    "Elixir.ZcashBackendKeys",
+    [from_bytes, from_seed, to_account_pubkey],
+    load = load
+);
