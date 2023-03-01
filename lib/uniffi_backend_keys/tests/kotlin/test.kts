e53518efae41537e1e239e534f7ad0d21071e394
eloylp <eloylp@eloylp.dev>
2023-03-01 17:06:06 +0100

    refactor: prepare the code avoid name collisions

--- lib/uniffi_backend_keys/tests/kotlin/test.kts
+++ lib/uniffi_backend_keys/tests/kotlin/test.kts
@@ -12,7 +12,7 @@ val unifiedSpendingKey = ZcashUnifiedSpendingKey.fromSeed(
 
 val encodedUnifiedFullViewingKey = "uview1ac6swpuurz2cgr8ktk630exjrz45fsuc4jeqwgg4dm33stl8awhcju0kyaxvw58405jla4k7rqfcw35l4rsj3ta74a2me8p9hh52uxp5zm5wk60pkpy7242wdhdgm265ah3pjqe03m0vax0wa2k4yqnu0gzmnnkt2sjmxeg7s3v8j55mnrzwqznttkaj86ghs2hzp0pstlvw4zlc7kqc2n98h6xluat24829f5fvgue0w8m9r2fwtyzrdvxf7vwu67fd0wdtc0m3m952prz3w7sc8s42v48u9nsd4gld2pgjfzu9qxxxs06mdtkz2dcda0926wulk0t564k3gs6mjm04qmj6e2yrj8vmjh3flh6fg7y4k5fjj09xmv2ffv6ua7e97fszgfpp94uytsq0cu35dd53n45ua4m43gha3dquw60as4xrynllveyjczyffsd8fm6npe88pmg6j6kpjfapuurnrwjya3gz2xfvmyv5r433rsnkra8h"
 
-val diversifier = ZcashDiversifier(listOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0).map { it.toUByte() })
+val saplingDiversifier = ZcashDiversifier(listOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0).map { it.toUByte() })
 
 val saplingIvkPaymentAddressBytes = listOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 150, 127, 9, 126, 127, 135, 242, 241, 100, 51, 242, 226, 238, 170, 123, 25, 163, 69, 216, 183, 101, 10, 82, 150, 119, 1, 188, 11, 103, 156, 95).map { it.toUByte() };
 
@@ -34,7 +34,7 @@ assert(unifiedSpendingKey.toUnifiedFullViewingKey()
 // Test ZcashSaplingIvk.toPaymentAddress
 assert(unifiedSpendingKey.toUnifiedFullViewingKey()
     .sapling()!!.toIvk(ZcashScope.EXTERNAL)
-    .toPaymentAddress(diversifier)!!.toBytes() == saplingIvkPaymentAddressBytes)
+    .toPaymentAddress(saplingDiversifier)!!.toBytes() == saplingIvkPaymentAddressBytes)
 
 // Test ZcashAccountPrivKey.fromSeed
 assert(accountPrivKey.toBytes() == accountPrivKeyBytes)
