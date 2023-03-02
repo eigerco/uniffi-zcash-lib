5bc4d1c935e7971137af46d6c2c519c89e564ee8
Łukasz Niemier <lukasz@eiger.co>
2023-03-02 19:03:51 +0100

    docs: add documentation to transparent address

--- lib/uniffi_backend_keys/src/transparent.rs
+++ lib/uniffi_backend_keys/src/transparent.rs
@@ -1,4 +1,4 @@
-use zcash_primitives::legacy;
+use zcash_primitives::legacy::{self, TransparentAddress::*};
 
 /// A transparent address corresponding to either a public key or a script.
 pub struct ZcashTransparentAddress(legacy::TransparentAddress);
@@ -19,12 +19,36 @@ impl ZcashTransparentAddress {
     /// Create new transparent address corresponding to public key
     pub fn public_key(input: Vec<u8>) -> crate::ZcashResult<Self> {
         let buf = crate::utils::cast_slice(&input)?;
-        Ok(legacy::TransparentAddress::PublicKey(buf).into())
+        Ok(PublicKey(buf).into())
     }
 
     /// Create new transparent address corresponding to script
     pub fn script(input: Vec<u8>) -> crate::ZcashResult<Self> {
         let buf = crate::utils::cast_slice(&input)?;
-        Ok(legacy::TransparentAddress::Script(buf).into())
+        Ok(Script(buf).into())
+    }
+
+    /// Check if it is public key transparent address
+    pub fn is_public_key(&self) -> bool {
+        match self.0 {
+            PublicKey(_) => true,
+            _ => false,
+        }
+    }
+
+    /// Check if it is script transparent address
+    pub fn is_script(&self) -> bool {
+        match self.0 {
+            Script(_) => true,
+            _ => false,
+        }
+    }
+
+    /// Return raw bytes corresponding to given address
+    pub fn to_bytes(&self) -> Vec<u8> {
+        match self.0 {
+            PublicKey(bytes) => bytes.into(),
+            Script(bytes) => bytes.into(),
+        }
     }
 }
