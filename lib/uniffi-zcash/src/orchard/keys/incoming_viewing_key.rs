40c4ec0c2890d23b34541a63005d7616478cbc7f
Daniel Zduniak <dzduniak@gmail.com>
2023-03-20 17:32:17 +0100

    ft: complete ZcashIncomingViewingKey implementation

--- lib/uniffi-zcash/src/orchard/keys/incoming_viewing_key.rs
+++ lib/uniffi-zcash/src/orchard/keys/incoming_viewing_key.rs
@@ -1,8 +1,11 @@
 use std::sync::Arc;
 
-use orchard::keys::IncomingViewingKey;
+use orchard::keys::{DiversifierIndex, IncomingViewingKey};
 
-use crate::{ZcashOrchardAddress, ZcashOrchardDiversifier};
+use crate::{
+    utils, ZcashError, ZcashOrchardAddress, ZcashOrchardDiversifier, ZcashOrchardDiversifierIndex,
+    ZcashResult,
+};
 
 /// A key that provides the capability to detect and decrypt incoming notes from the block
 /// chain, without being able to spend the notes or detect when they are spent.
@@ -12,10 +15,42 @@ use crate::{ZcashOrchardAddress, ZcashOrchardDiversifier};
 pub struct ZcashOrchardIncomingViewingKey(IncomingViewingKey);
 
 impl ZcashOrchardIncomingViewingKey {
+    /// Serializes an Orchard incoming viewing key to its raw encoding as specified in [Zcash Protocol Spec § 5.6.4.3: Orchard Raw Incoming Viewing Keys][orchardrawinviewingkeys]
+    ///
+    /// [orchardrawinviewingkeys]: https://zips.z.cash/protocol/protocol.pdf#orchardinviewingkeyencoding
     pub fn to_bytes(&self) -> Vec<u8> {
         self.0.to_bytes().to_vec()
     }
 
+    /// Parses an Orchard incoming viewing key from its raw encoding.
+    pub fn from_bytes(bytes: Vec<u8>) -> ZcashResult<Self> {
+        let array = utils::cast_slice(&bytes)?;
+        let key: Option<_> = IncomingViewingKey::from_bytes(&array).into();
+        Ok(ZcashOrchardIncomingViewingKey(
+            key.ok_or(ZcashError::Unknown)?,
+        ))
+    }
+
+    /// Checks whether the given address was derived from this incoming viewing
+    /// key, and returns the diversifier index used to derive the address if
+    /// so. Returns `None` if the address was not derived from this key.
+    pub fn diversifier_index(
+        &self,
+        addr: Arc<ZcashOrchardAddress>,
+    ) -> Option<Arc<ZcashOrchardDiversifierIndex>> {
+        self.0
+            .diversifier_index(&addr.as_ref().into())
+            .map(From::from)
+            .map(Arc::new)
+    }
+
+    /// Returns the payment address for this key at the given index.
+    pub fn address_at(&self, j: Arc<ZcashOrchardDiversifierIndex>) -> Arc<ZcashOrchardAddress> {
+        let j: DiversifierIndex = j.as_ref().into();
+        Arc::new(self.0.address_at(j).into())
+    }
+
+    /// Returns the payment address for this key corresponding to the given diversifier.
     pub fn address(&self, diversifier: Arc<ZcashOrchardDiversifier>) -> Arc<ZcashOrchardAddress> {
         Arc::new(self.0.address(diversifier.0).into())
     }
