fba4216456092d9e7b5d3ce2f68df7ae86546fe2
Luca Campobasso <lcampobasso@gmail.com>
2023-04-03 14:21:57 +0200

    moved some tests

--- lib/uniffi-zcash/tests/kotlin/zcash_primitives/sapling/keys/outgoing_viewing_key.kts
+++ lib/uniffi-zcash/tests/kotlin/zcash_primitives/sapling/keys/outgoing_viewing_key.kts
@@ -8,3 +8,22 @@ fun testOutgoingViewingKeyFromBytes() {
 	assert(ovk.toBytes() == expectedBytes)
 }
 testOutgoingViewingKeyFromBytes()
+
+fun testSaplingOvkToBytes() {
+	val supp = TestSupport.fromCsvFile()
+	val seed = supp.getArgumentAsByteArray("seed")
+
+    val unifiedSpendingKey = ZcashUnifiedSpendingKey.fromSeed(
+        ZcashConsensusParameters.MAIN_NETWORK,
+        seed,
+        ZcashAccountId(0u),
+    )
+
+    val expectedBytes = supp.getAsByteArray("sapling_outgoing_viewing_key")
+
+    val ovkBytes = unifiedSpendingKey.toUnifiedFullViewingKey()
+        .sapling()!!.toOvk(ZcashScope.EXTERNAL).toBytes()
+
+    assert(ovkBytes == expectedBytes)
+}
+testSaplingOvkToBytes()
