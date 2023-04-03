80256c8328603c691ac9801d1fa20ceb9653f0be
Luca Campobasso <lcampobasso@gmail.com>
2023-04-03 14:21:57 +0200

    moved some tests

--- lib/uniffi-zcash/tests/kotlin/orchard/address.kts
+++ lib/uniffi-zcash/tests/kotlin/orchard/address.kts
@@ -19,3 +19,25 @@ fun testOrchardAddressDiversifier() {
 	assert(diver == expectedBytes)
 }
 testOrchardAddressDiversifier()
+
+fun testOrchardIvkToPaymentAddress() {
+	val supp = TestSupport.fromCsvFile()
+	val seed = supp.getArgumentAsByteArray("seed")
+
+    val unifiedSpendingKey = ZcashUnifiedSpendingKey.fromSeed(
+        ZcashConsensusParameters.MAIN_NETWORK,
+        seed,
+        ZcashAccountId(0u),
+    )
+
+    val orchardDiversifier = ZcashOrchardDiversifier.fromBytes(listOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0).map { it.toUByte() })
+
+    val expectedBytes = supp.getAsByteArray("orchard_address")
+
+    val rawAddressBytes = unifiedSpendingKey.toUnifiedFullViewingKey()
+        .orchard()!!.toIvk(ZcashOrchardScope.EXTERNAL)
+        .address(orchardDiversifier)!!.toRawAddressBytes()
+
+    assert(rawAddressBytes == expectedBytes)
+}
+testOrchardIvkToPaymentAddress()
