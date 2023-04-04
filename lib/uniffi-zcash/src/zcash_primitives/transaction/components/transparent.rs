e8cf58d8ef964cc1dd15ef178a9d019f58f12966
eloylp <eloylp@eloylp.dev>
2023-04-04 17:17:48 +0200

    Complete ZcashTxOut type

--- lib/uniffi-zcash/src/zcash_primitives/transaction/components/transparent.rs
+++ lib/uniffi-zcash/src/zcash_primitives/transaction/components/transparent.rs
@@ -34,6 +34,13 @@ impl From<&ZcashOutPoint> for OutPoint {
 pub struct ZcashTxOut(TxOut);
 
 impl ZcashTxOut {
+    pub fn new(value: Arc<ZcashAmount>, script_pubkey: Arc<ZcashScript>) -> Self {
+        Self(TxOut {
+            value: value.as_ref().into(),
+            script_pubkey: script_pubkey.as_ref().into(),
+        })
+    }
+
     pub fn value(&self) -> Arc<ZcashAmount> {
         Arc::new(self.0.value.into())
     }
