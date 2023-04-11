fbb62445dbe4ae90949a2c2bef66404a7a6009ea
eloylp <eloylp@eloylp.dev>
2023-04-11 09:11:38 +0200

    Add initial example of orchard transaction

--- lib/uniffi-zcash/src/bin/transaction.rs
+++ lib/uniffi-zcash/src/bin/transaction.rs
@@ -1,6 +1,16 @@
+use hdwallet::rand_core::OsRng;
+use orchard::{
+    builder::{InProgress, Unauthorized, Unproven},
+    bundle::Flags,
+    keys::Diversifier,
+    note::{Nullifier, RandomSeed},
+    tree::{MerkleHashOrchard, MerklePath},
+    value::NoteValue,
+    Note,
+};
 use zcash_client_backend::keys::UnifiedSpendingKey;
 use zcash_primitives::{
-    consensus::{BlockHeight, MainNetwork},
+    consensus::{BlockHeight, BranchId, MainNetwork},
     legacy::keys::IncomingViewingKey,
     memo::MemoBytes,
     merkle_tree::{CommitmentTree, IncrementalWitness},
@@ -9,9 +19,10 @@ use zcash_primitives::{
         builder::Builder,
         components::{Amount, OutPoint, TxOut},
         fees::{fixed, zip317},
+        Authorized, TransactionData, TxVersion,
     },
-    zip32::Scope,
 };
+
 use zcash_proofs::prover::LocalTxProver;
 
 const BLOCK_HEIGHT: u32 = 2030820;
@@ -32,6 +43,9 @@ fn main() {
 
     println!();
     sapling_transaction_general_builder_example(&key);
+
+    println!();
+    orchard_transaction(&key);
 }
 
 pub fn transparent_builder_with_nonstandard_fee_example(key: &UnifiedSpendingKey) {
@@ -224,7 +238,7 @@ pub fn sapling_transaction_general_builder_example(key: &UnifiedSpendingKey) {
             Some(
                 key.sapling()
                     .to_diversifiable_full_viewing_key()
-                    .to_ovk(Scope::Internal),
+                    .to_ovk(zcash_primitives::zip32::Scope::Internal),
             ),
             payment_address,
             Amount::from_u64(200).unwrap(),
@@ -240,6 +254,82 @@ pub fn sapling_transaction_general_builder_example(key: &UnifiedSpendingKey) {
     transaction.write(&mut data).unwrap();
     println!(
         "Sapling transaction data (from general builder) len: {}",
+        data.len()
+    );
+    println!("_______________________________________________");
+    println!(
+        "[{}]",
+        data.iter()
+            .map(|byte| byte.to_string())
+            .collect::<Vec<String>>()
+            .join(", ")
+    );
+}
+
+pub fn orchard_transaction(key: &UnifiedSpendingKey) {
+    // Key derivation
+    let ufvk = key.to_unified_full_viewing_key();
+    let fvk = ufvk.orchard().unwrap();
+    let ovk = fvk.to_ovk(orchard::keys::Scope::External);
+    let address = fvk
+        .to_ivk(orchard::keys::Scope::Internal)
+        .address(Diversifier::from_bytes([0u8; 11]));
+
+    // Note construction
+    let note_value = NoteValue::from_raw(15);
+    let nullifier = Nullifier::from_bytes(&[0u8; 32]).unwrap();
+    let rseed = RandomSeed::from_bytes([0u8; 32], &nullifier).unwrap();
+    let note = Note::from_parts(address, note_value, nullifier, rseed).unwrap();
+
+    let mut auth_path = [MerkleHashOrchard::from_bytes(&[0u8; 32]).unwrap(); 32];
+    auth_path[1] = MerkleHashOrchard::from_bytes(&[0u8; 32]).unwrap();
+
+    let merkle_path = MerklePath::from_parts(0, auth_path);
+    let anchor = merkle_path.root(note.commitment().into());
+
+    let spends_enabled = true;
+    let outputs_enabled = true;
+
+    let mut builder =
+        orchard::builder::Builder::new(Flags::from_parts(spends_enabled, outputs_enabled), anchor);
+
+    builder
+        .add_recipient(Some(ovk), address, note_value, None)
+        .unwrap();
+
+    builder.add_spend(fvk.clone(), note, merkle_path).unwrap();
+
+    let bundle: orchard::Bundle<InProgress<Unproven, Unauthorized>, Amount> =
+        builder.build(OsRng).unwrap();
+
+    let pk = orchard::circuit::ProvingKey::build();
+    let sighash = [0u8; 32]; // External param
+    let proved_bundle = bundle.create_proof(&pk, OsRng).unwrap();
+    let authorized_bundle = proved_bundle
+        .apply_signatures(OsRng, sighash, &[key.orchard().into()])
+        .unwrap();
+
+    let consensus_branch_id =
+        BranchId::for_height(&MainNetwork, BlockHeight::from_u32(BLOCK_HEIGHT));
+    let version = TxVersion::suggested_for_branch(consensus_branch_id);
+
+    let transaction_data: TransactionData<Authorized> = TransactionData::from_parts(
+        version,
+        consensus_branch_id,
+        23,
+        BlockHeight::from_u32(BLOCK_HEIGHT + 100),
+        None,
+        None,
+        None,
+        Some(authorized_bundle),
+    );
+
+    let transaction = transaction_data.freeze().unwrap();
+
+    let mut data = Vec::new();
+    transaction.write(&mut data).unwrap();
+    println!(
+        "Orchard transaction data (from specific builder) len: {}",
         data.len()
     );
     println!("_______________________________________________");
