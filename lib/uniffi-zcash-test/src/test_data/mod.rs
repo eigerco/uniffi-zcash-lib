29983622e2f6e50127856e0b4e3983ca1d707f56
Daniel Zduniak <dzduniak@gmail.com>
2023-04-20 10:47:44 +0200

    ft: generate test data in build script

--- lib/uniffi-zcash-test/src/test_data/mod.rs
+++ lib/uniffi-zcash-test/src/test_data/mod.rs
@@ -7,6 +7,7 @@ mod zcash_primitives;
 
 use std::fs::OpenOptions;
 use std::io::Write;
+use std::path::Path;
 
 use self::hdwallet::write_for_hdwallet;
 use self::orchard::write_for_orchard;
@@ -15,27 +16,31 @@ use self::transaction::write_for_transaction;
 use self::zcash_client_backend::write_for_zcash_client_backend;
 use self::zcash_primitives::write_for_zcash_primitives;
 
-pub fn generate_test_data() {
+pub fn generate_test_data(force: bool) {
     let base_url = env!("CARGO_MANIFEST_DIR");
-    let mut file = OpenOptions::new()
-        .create(true)
-        .write(true)
-        .truncate(true)
-        .open(format!("{base_url}/../uniffi-zcash/tests/test_data.csv"))
-        .unwrap();
+    let path = format!("{base_url}/../uniffi-zcash/tests/test_data.csv");
 
-    writeln!(file, "test_string:TestString").unwrap();
+    if force || !Path::new(&path).exists() {
+        let mut file = OpenOptions::new()
+            .create(true)
+            .write(true)
+            .truncate(true)
+            .open(path)
+            .unwrap();
 
-    let mut seed = vec![0u8; 32];
-    seed[0] = 1u8;
-    writeln!(file, "{}", format_bytes("seed", &seed)).unwrap();
+        writeln!(file, "test_string:TestString").unwrap();
 
-    write_for_hdwallet(&mut file, &seed);
-    write_for_orchard(&mut file, &seed);
-    write_for_secp256k1(&mut file, &seed);
-    write_for_zcash_client_backend(&mut file, &seed);
-    write_for_zcash_primitives(&mut file, &seed);
-    write_for_transaction(&mut file, &seed);
+        let mut seed = vec![0u8; 32];
+        seed[0] = 1u8;
+        writeln!(file, "{}", format_bytes("seed", &seed)).unwrap();
+
+        write_for_hdwallet(&mut file, &seed);
+        write_for_orchard(&mut file, &seed);
+        write_for_secp256k1(&mut file, &seed);
+        write_for_zcash_client_backend(&mut file, &seed);
+        write_for_zcash_primitives(&mut file, &seed);
+        write_for_transaction(&mut file, &seed);
+    }
 }
 
 pub(crate) fn format_bytes(label: &str, bytes: &[u8]) -> String {
