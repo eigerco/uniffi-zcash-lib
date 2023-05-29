2d5982bb520c94bf01ee0f13584ab78b5a12979d
eloylp <eloylp@eloylp.dev>
2023-05-29 19:43:57 +0200

    CLI: Make load env script more robust

--- lib/uniffi-zcash-cli/load_env.sh
+++ lib/uniffi-zcash-cli/load_env.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 if [ -f $SCRIPT_DIR/.env ]; then
-  export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
+  export $(echo $(cat $SCRIPT_DIR/.env | sed 's/#.*//g'| xargs) | envsubst)
+else
+  echo "could not find .env file at ${SCRIPT_DIR}"
 fi
+
