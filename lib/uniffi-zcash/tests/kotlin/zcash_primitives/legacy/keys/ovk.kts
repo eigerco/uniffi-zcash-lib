6551211dc75ab3edeb43c26f59ee951734d1cbbd
eloylp <eloylp@eloylp.dev>
2023-06-07 20:25:34 +0200

    Improve test performance, group all kts in the same file

--- lib/uniffi-zcash/tests/kotlin/zcash_primitives/legacy/keys/ovk.kts
+++
@@ -1,9 +0,0 @@
-fun testExternalOvkAsBytes() {
-    // covered in account_pub_key
-}
-testExternalOvkAsBytes()
-
-fun testInternalOvkAsBytes() {
-    // covered in account_pub_key
-}
-testInternalOvkAsBytes()
