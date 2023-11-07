bcc3c841481959cabeecaddaad63e1d17226c94f
Luca Campobasso <lcampobasso@gmail.com>
2023-11-07 22:41:30 +0100

    temporarily remove git

--- lib/uniffi-zcash-cli/src/diff/git.rs
+++ lib/uniffi-zcash-cli/src/diff/git.rs
@@ -1,7 +1,7 @@
 use std::path::Path;
 
 use anyhow::Context;
-use git2::Repository;
+// use git2::Repository;
 
 const LIBS_REPO_URL: &str = "https://github.com/zcash/librustzcash.git";
 
@@ -14,31 +14,31 @@ pub(crate) fn init_libs_repo(
     version: &str,
 ) -> anyhow::Result<()> {
     let tag = format!("{}-{}", lib_name, version);
-    let repo = Repository::open(repo_path)
-        .or_else(|_| Repository::clone(LIBS_REPO_URL, repo_path))
-        .context("failed to clone current repo")?;
+    // let repo = Repository::open(repo_path)
+    //     .or_else(|_| Repository::clone(LIBS_REPO_URL, repo_path))
+    //     .context("failed to clone current repo")?;
 
-    checkout_tag(&repo, &tag)?;
+    // checkout_tag(&repo, &tag)?;
 
     Ok(())
 }
 
 // Chckout a certain tag from a given repository
-fn checkout_tag(repo: &git2::Repository, tag: &str) -> anyhow::Result<()> {
-    let (object, reference) = repo
-        .revparse_ext(tag)
-        .with_context(|| format!("git object \"{}\" not found", tag))?;
+// fn checkout_tag(repo: &git2::Repository, tag: &str) -> anyhow::Result<()> {
+//     let (object, reference) = repo
+//         .revparse_ext(tag)
+//         .with_context(|| format!("git object \"{}\" not found", tag))?;
 
-    repo.checkout_tree(&object, None)
-        .with_context(|| format!("failed to checkout tree for \"{}\"", tag))?;
+//     repo.checkout_tree(&object, None)
+//         .with_context(|| format!("failed to checkout tree for \"{}\"", tag))?;
 
-    match reference {
-        // gref is an actual reference like branches or tags
-        Some(gref) => repo.set_head(gref.name().unwrap()),
-        // this is a commit, not a reference
-        None => repo.set_head_detached(object.id()),
-    }
-    .with_context(|| format!("failed to set HEAD for \"{}\"", tag))?;
+//     match reference {
+//         // gref is an actual reference like branches or tags
+//         Some(gref) => repo.set_head(gref.name().unwrap()),
+//         // this is a commit, not a reference
+//         None => repo.set_head_detached(object.id()),
+//     }
+//     .with_context(|| format!("failed to set HEAD for \"{}\"", tag))?;
 
-    Ok(())
-}
+//     Ok(())
+// }
