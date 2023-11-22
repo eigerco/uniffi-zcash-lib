bb29457a76cc9675bfbb5454eda126f47f4ba69c
Luca Campobasso <lcampobasso@gmail.com>
2023-11-22 11:45:02 +0100

    added zip317 input selection methods

--- lib/uniffi-zcash/src/zcash_client_backend/data_api/wallet/input_selection.rs
+++ lib/uniffi-zcash/src/zcash_client_backend/data_api/wallet/input_selection.rs
@@ -2,28 +2,33 @@ use std::sync::{Arc, Mutex};
 
 use rusqlite::Connection;
 use zcash_client_backend::data_api::wallet::input_selection::GreedyInputSelector;
-use zcash_client_backend::fees::fixed::SingleOutputChangeStrategy;
+use zcash_client_backend::fees::fixed::SingleOutputChangeStrategy as FixedSingleOutputChangeStrategy;
+use zcash_client_backend::fees::zip317::SingleOutputChangeStrategy as Zip317SingleOutputChangeStrategy;
 use zcash_client_sqlite::WalletDb;
 use zcash_primitives::consensus::{MainNetwork, TestNetwork};
 
-use crate::{ZcashDustOutputPolicy, ZcashFixedSingleOutputChangeStrategy};
+use crate::{
+    ZcashDustOutputPolicy, ZcashFixedSingleOutputChangeStrategy,
+    ZcashZip317SingleOutputChangeStrategy,
+};
 
-pub type MainGreedyInputSelector =
-    GreedyInputSelector<WalletDb<Connection, MainNetwork>, SingleOutputChangeStrategy>;
+// Fixed
 
-pub struct ZcashMainGreedyInputSelector {
-    internal: Mutex<MainGreedyInputSelector>,
+pub type MainFixedGreedyInputSelector =
+    GreedyInputSelector<WalletDb<Connection, MainNetwork>, FixedSingleOutputChangeStrategy>;
+
+pub struct ZcashMainFixedGreedyInputSelector {
+    internal: Mutex<MainFixedGreedyInputSelector>,
     change_strategy: ZcashFixedSingleOutputChangeStrategy,
     dust_output_policy: ZcashDustOutputPolicy,
 }
 
-impl ZcashMainGreedyInputSelector {
-    // use trait to generalize ZcashSingleOutputChangeStrategy
+impl ZcashMainFixedGreedyInputSelector {
     pub fn new(
         change_strategy: Arc<ZcashFixedSingleOutputChangeStrategy>,
         dust_output_policy: Arc<ZcashDustOutputPolicy>,
     ) -> Self {
-        let insel: MainGreedyInputSelector = GreedyInputSelector::new(
+        let insel: MainFixedGreedyInputSelector = GreedyInputSelector::new(
             (*change_strategy).clone().into(),
             (*dust_output_policy).into(),
         );
@@ -35,7 +40,7 @@ impl ZcashMainGreedyInputSelector {
     }
 }
 
-impl Clone for ZcashMainGreedyInputSelector {
+impl Clone for ZcashMainFixedGreedyInputSelector {
     fn clone(&self) -> Self {
         Self::new(
             Arc::new(self.change_strategy.clone()),
@@ -44,28 +49,27 @@ impl Clone for ZcashMainGreedyInputSelector {
     }
 }
 
-impl From<ZcashMainGreedyInputSelector> for MainGreedyInputSelector {
-    fn from(outer: ZcashMainGreedyInputSelector) -> Self {
+impl From<ZcashMainFixedGreedyInputSelector> for MainFixedGreedyInputSelector {
+    fn from(outer: ZcashMainFixedGreedyInputSelector) -> Self {
         outer.internal.into_inner().unwrap()
     }
 }
 
-pub type TestGreedyInputSelector =
-    GreedyInputSelector<WalletDb<Connection, TestNetwork>, SingleOutputChangeStrategy>;
+pub type TestFixedGreedyInputSelector =
+    GreedyInputSelector<WalletDb<Connection, TestNetwork>, FixedSingleOutputChangeStrategy>;
 
-pub struct ZcashTestGreedyInputSelector {
-    internal: Mutex<TestGreedyInputSelector>,
+pub struct ZcashTestFixedGreedyInputSelector {
+    internal: Mutex<TestFixedGreedyInputSelector>,
     change_strategy: ZcashFixedSingleOutputChangeStrategy,
     dust_output_policy: ZcashDustOutputPolicy,
 }
 
-impl ZcashTestGreedyInputSelector {
-    // use trait to generalize ZcashSingleOutputChangeStrategy
+impl ZcashTestFixedGreedyInputSelector {
     pub fn new(
         change_strategy: Arc<ZcashFixedSingleOutputChangeStrategy>,
         dust_output_policy: Arc<ZcashDustOutputPolicy>,
     ) -> Self {
-        let insel: TestGreedyInputSelector = GreedyInputSelector::new(
+        let insel: TestFixedGreedyInputSelector = GreedyInputSelector::new(
             (*change_strategy).clone().into(),
             (*dust_output_policy).into(),
         );
@@ -77,7 +81,7 @@ impl ZcashTestGreedyInputSelector {
     }
 }
 
-impl Clone for ZcashTestGreedyInputSelector {
+impl Clone for ZcashTestFixedGreedyInputSelector {
     fn clone(&self) -> Self {
         Self::new(
             Arc::new(self.change_strategy.clone()),
@@ -86,8 +90,92 @@ impl Clone for ZcashTestGreedyInputSelector {
     }
 }
 
-impl From<ZcashTestGreedyInputSelector> for TestGreedyInputSelector {
-    fn from(outer: ZcashTestGreedyInputSelector) -> Self {
+impl From<ZcashTestFixedGreedyInputSelector> for TestFixedGreedyInputSelector {
+    fn from(outer: ZcashTestFixedGreedyInputSelector) -> Self {
+        outer.internal.into_inner().unwrap()
+    }
+}
+
+// ZIP317
+
+pub type MainZip317GreedyInputSelector =
+    GreedyInputSelector<WalletDb<Connection, MainNetwork>, Zip317SingleOutputChangeStrategy>;
+
+pub struct ZcashMainZip317GreedyInputSelector {
+    internal: Mutex<MainZip317GreedyInputSelector>,
+    change_strategy: ZcashZip317SingleOutputChangeStrategy,
+    dust_output_policy: ZcashDustOutputPolicy,
+}
+
+impl ZcashMainZip317GreedyInputSelector {
+    pub fn new(
+        change_strategy: Arc<ZcashZip317SingleOutputChangeStrategy>,
+        dust_output_policy: Arc<ZcashDustOutputPolicy>,
+    ) -> Self {
+        let insel: MainZip317GreedyInputSelector = GreedyInputSelector::new(
+            (*change_strategy).clone().into(),
+            (*dust_output_policy).into(),
+        );
+        Self {
+            internal: Mutex::new(insel),
+            change_strategy: (*change_strategy).clone(),
+            dust_output_policy: *dust_output_policy,
+        }
+    }
+}
+
+impl Clone for ZcashMainZip317GreedyInputSelector {
+    fn clone(&self) -> Self {
+        Self::new(
+            Arc::new(self.change_strategy.clone()),
+            Arc::new(self.dust_output_policy),
+        )
+    }
+}
+
+impl From<ZcashMainZip317GreedyInputSelector> for MainZip317GreedyInputSelector {
+    fn from(outer: ZcashMainZip317GreedyInputSelector) -> Self {
+        outer.internal.into_inner().unwrap()
+    }
+}
+
+pub type TestZip317GreedyInputSelector =
+    GreedyInputSelector<WalletDb<Connection, TestNetwork>, Zip317SingleOutputChangeStrategy>;
+
+pub struct ZcashTestZip317GreedyInputSelector {
+    internal: Mutex<TestZip317GreedyInputSelector>,
+    change_strategy: ZcashZip317SingleOutputChangeStrategy,
+    dust_output_policy: ZcashDustOutputPolicy,
+}
+
+impl ZcashTestZip317GreedyInputSelector {
+    pub fn new(
+        change_strategy: Arc<ZcashZip317SingleOutputChangeStrategy>,
+        dust_output_policy: Arc<ZcashDustOutputPolicy>,
+    ) -> Self {
+        let insel: TestZip317GreedyInputSelector = GreedyInputSelector::new(
+            (*change_strategy).clone().into(),
+            (*dust_output_policy).into(),
+        );
+        Self {
+            internal: Mutex::new(insel),
+            change_strategy: (*change_strategy).clone(),
+            dust_output_policy: *dust_output_policy,
+        }
+    }
+}
+
+impl Clone for ZcashTestZip317GreedyInputSelector {
+    fn clone(&self) -> Self {
+        Self::new(
+            Arc::new(self.change_strategy.clone()),
+            Arc::new(self.dust_output_policy),
+        )
+    }
+}
+
+impl From<ZcashTestZip317GreedyInputSelector> for TestZip317GreedyInputSelector {
+    fn from(outer: ZcashTestZip317GreedyInputSelector) -> Self {
         outer.internal.into_inner().unwrap()
     }
 }
--- lib/uniffi-zcash/src/zcash_client_backend/fees/zip317.rs
+++ lib/uniffi-zcash/src/zcash_client_backend/fees/zip317.rs
@@ -1,6 +1,7 @@
 use std::sync::Arc;
 
 use zcash_client_backend::fees::zip317::SingleOutputChangeStrategy;
+use zcash_client_backend::fees::ChangeStrategy;
 
 use crate::ZcashZip317FeeRule;
 
@@ -11,5 +12,24 @@ impl ZcashZip317SingleOutputChangeStrategy {
     /// fee parameters.
     pub fn new(fee_rule: Arc<ZcashZip317FeeRule>) -> Self {
         Self(SingleOutputChangeStrategy::new((*fee_rule).clone().into()))
+    }
+}
+
+impl From<SingleOutputChangeStrategy> for ZcashZip317SingleOutputChangeStrategy {
+    fn from(inner: SingleOutputChangeStrategy) -> Self {
+        ZcashZip317SingleOutputChangeStrategy(inner)
+    }
+}
+
+impl From<ZcashZip317SingleOutputChangeStrategy> for SingleOutputChangeStrategy {
+    fn from(outer: ZcashZip317SingleOutputChangeStrategy) -> Self {
+        outer.0
+    }
+}
+
+impl Clone for ZcashZip317SingleOutputChangeStrategy {
+    fn clone(&self) -> Self {
+        let strategy: &SingleOutputChangeStrategy = &self.0;
+        Self(SingleOutputChangeStrategy::new(strategy.fee_rule().clone()))
     }
 }
